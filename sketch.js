a4ca9f8bfa57b04b3589730b3c5242ed2c03b6eb
M. Can Kandemir <cnkndmr@gmail.com>
2019-09-29 14:13:58 +0300

    Buttons replaced with sliders

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@
 */
 var particles = [];
 
+var rows = [];
+var row_count;
+
 var rx;
 var ry;
 var rh;
@@ -18,17 +21,13 @@ var vert_v = 0.5;
 var par_count = 1;
 var coef;
 
-var button1;
-var button2;
-var button3;
-var button4;
-var button5;
-var button6;
+var slider1;
+var slider2;
+var slider3;
+
+var button;
 var buttonposx = 10;
 var buttonposy = 335;
-
-var rows = [];
-var row_count;
 
 function setup() {
 	createCanvas(600, 400);
@@ -37,31 +36,19 @@ function setup() {
 	for (var i = 0; i < row_count; i++) {
 		rows[i] = 0;
 	}
-	createDiv('<p><h1>Single-slit diffraction and the uncertainty principle.</h1><h3>Controls:</h3><b>W Key</b>: Increase the slit width.<br><b>S Key</b>: Decrease the slit width.<br><b>D Key</b>: Increase the particle count.<br>(<b>CAUTION</b>: Increasing particle count may cause slow downs on computer!)<br><b>A Key</b>: Decrease the particle count.<br><b>1 Key</b>: Increase the particle gun range.<br><b>2 Key</b>: Decrease the particle gun range.<br></p>');
-	button1 = createButton("w");
-	button1.mousePressed(addslit);
-	button1.position(buttonposx + 30, buttonposy + 10);
-	button1.size(30, 30);
-	button2 = createButton("s");
-	button2.mousePressed(remslit);
-	button2.position(buttonposx + 30, buttonposy + 40);
-	button2.size(30, 30);
-	button3 = createButton("a");
-	button3.mousePressed(rempar);
-	button3.position(buttonposx, buttonposy + 40);
-	button3.size(30, 30);
-	button4 = createButton("d");
-	button4.mousePressed(addpar);
-	button4.position(buttonposx + 60, buttonposy + 40);
-	button4.size(30, 30);
-	button5 = createButton("1");
-	button5.mousePressed(addvel);
-	button5.position(buttonposx, buttonposy + 10);
-	button5.size(30, 30);
-	button6 = createButton("2");
-	button6.mousePressed(remvel);
-	button6.position(buttonposx + 60, buttonposy + 10);
-	button6.size(30, 30);
+	createDiv('<p><h1>Single-slit diffraction and the uncertainty principle.</h1><h3>Controls:</h3><b>Slider 1</b>: Increase/Decrease the slit width.<br><b>Slider 2</b>: Increase/Decrease the particle gun range.<br><b>Slider 3</b>: Increase/Decrease the particle count.<br>(<b>CAUTION</b>: Increasing particle count may cause slow downs on computer!)<br><br><a href="https://github.com/cnkndmr/single_slit_p5js">github.com/cnkndmr/single_slit_p5js</a></p>');
+	button = createButton("Reset");
+	button.mousePressed(resetpar);
+	button.position(buttonposx, buttonposy - 20);
+	slider1 = createSlider(0, 200, D, 0.5);
+	slider1.position(buttonposx, buttonposy + 10);
+	slider1.style('width', '290px');
+	slider2 = createSlider(0.2, 5, vert_v, 0.01);
+	slider2.position(buttonposx, buttonposy + 30);
+	slider2.style('width', '290px');
+	slider3 = createSlider(0, 20, 1);
+	slider3.position(buttonposx, buttonposy + 50);
+	slider3.style('width', '290px');
 }
 
 function draw() {
@@ -86,24 +73,6 @@ function draw() {
 			particles.splice(i, 1);
 		}
 	}
-	if (keyIsDown(50)) {
-		remvel();
-	}
-	if (keyIsDown(49)) {
-		addvel();
-	}
-	if (keyIsDown(68)) {
-		addpar();
-	}
-	if (keyIsDown(65)) {
-		rempar();
-	}
-	if (keyIsDown(87)) {
-		addslit();
-	}
-	if (keyIsDown(83)) {
-		remslit();
-	}
 	rect(rx, ry, rw, rh);
 	rect(rx, r2y, rw, rh - 2);
 	line(width / 20, height / 2, width / 3 * cos(atan(vert_v / minvx)) + width / 20, width / 3 * sin(atan(vert_v / minvx)) + height / 2);
@@ -120,40 +89,17 @@ function draw() {
 	} else {
 		text('Multiplier: ' + 1, 0, 48);
 	}
-
+	D = slider1.value();
+	vert_v = slider2.value();
+	par_count = slider3.value();
 }
 
-function addpar() {
-	par_count += 1;
-}
-
-function rempar() {
-	if (par_count > 0) {
-		par_count -= 1;
+function resetpar() {
+	for (var i = particles.length - 1; i >= 0; i--) {
+		particles.splice(i, 1);
 	}
-}
-
-function addslit() {
-	if (D < height / 2) {
-		D += 1;
-	}
-}
-
-function remslit() {
-	if (D > 0) {
-		D -= 1;
-	}
-}
-
-function addvel() {
-	if (vert_v < 5) {
-		vert_v += 0.01;
-	}
-}
-
-function remvel() {
-	if (vert_v > 0.2) {
-		vert_v -= 0.01;
+	for (var i = 0; i < row_count; i++) {
+		rows[i] = 0;
 	}
 }
 
